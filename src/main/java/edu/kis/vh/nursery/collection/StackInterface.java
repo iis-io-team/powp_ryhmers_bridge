6ea860bedff883eb14a44e204cdc56016600871c
Rafikus <Rafikus95@gmail.com>
2018-07-08 18:42:10 +0200

    3.1.10

--- src/main/java/edu/kis/vh/nursery/collection/StackInterface.java
+++ src/main/java/edu/kis/vh/nursery/collection/StackInterface.java
@@ -1,7 +1,9 @@
 package edu.kis.vh.nursery.collection;
 
 public interface StackInterface {
-    int LIST_IS_EMPTY_VALUE = -1;
+    int LIST_IS_EMPTY_VALUE = 0;
+    //Zmiany wprowadzone w zadaniu 3.1.10 definitywnie pomogły. Jeżeli byłoby więcej niż dwie klasy które implementują
+    //ten interfejs trzeba by było w każdej klasie uwzględniać te zmiany.
 
     void push(int i);
 
